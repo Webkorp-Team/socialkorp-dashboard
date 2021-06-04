a36c33ce5746b5dafc7c3d234776f4c97769b8af
Mateus Rachid <mateus_rachid@outlook.com>
2021-06-04 01:32:44 -0300

    Added Webkorp-Team/styled-components-responsive-toolkit

---
+++ src/css-config/viewport.js
@@ -0,0 +1,15 @@
+const viewPortSizes = {
+  default: [1920,720],
+  mobile: [375,812],
+  tablet: [768,1024],
+};
+
+const pxToViewportAutoVhRatio = 1;
+
+const preserveAspectRatio = false;
+
+export default {
+  viewPortSizes,
+  pxToViewportAutoVhRatio,
+  preserveAspectRatio,
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,22 @@
+import { StyleSheetManager } from 'styled-components';
 import Head from 'next/head';
 import { wrapper } from 'store/store';
 
-import GlobalStyles from 'styles/global';
+import initResponsiveToolkit from 'styled-components-responsive-toolkit';
+import cssReset from 'css-config/css-reset';
+import viewport from 'css-config/viewport';
+import fonts from 'css-config/fonts';
+import mediaQueries from 'css-config/media-queries';
+
+const {
+  GlobalStyles,
+  plugins,
+} = initResponsiveToolkit({
+  cssReset,
+  viewport,
+  fonts,
+  mediaQueries,
+}); 
 
 function App({ Component, pageProps }) {
   return (
@@ -12,7 +27,9 @@ function App({ Component, pageProps }) {
         <meta name="description" content="A simple project starter with Nextjs" />
       </Head>
       <GlobalStyles/>
-      <Component {...pageProps} />
+      <StyleSheetManager stylisPlugins={plugins}>
+        <Component {...pageProps} />
+      </StyleSheetManager>
     </>
   )
 }
