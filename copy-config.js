26337812db0e2108b3759640f7ba0043a7edf69c
Mateus Rachid <mateus_rachid@outlook.com>
2021-08-28 15:34:48 -0300

    improved config files handling

---
+++ copy-config.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const cp = require('child_process');
+
+if(fs.existsSync('../website.config.yaml'))
+  cp.fork('parse-and-copy-config.js',{cwd:'..'});
+
+fs.copyFileSync('website.config.json','src/api/website.config.json');
+fs.copyFileSync('website.config.json','functions/website.config.json');
--- functions/genkey.sh
+++ functions/genkey.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-rm jwtRS256.key
-rm jwtRS256.key.pub
+if [ "$1" = "--no-overwrite" ] && [ -f jwtRS256.key ]; then
+  exit
+fi
+rm jwtRS256.key 2>/dev/null
+rm jwtRS256.key.pub 2>/dev/null
 ssh-keygen -t rsa -N "" -b 4096 -m PEM -f jwtRS256.key
 openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
