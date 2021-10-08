7bba77c1a1734d86816990b16d1308c7194af622
Mateus Rachid <mateus_rachid@outlook.com>
2021-10-07 22:38:20 -0300

    scripts refactoring

--- scripts/handle-config.js
+++ scripts/handle-config.js
@@ -1,8 +1,4 @@
 const fs = require('fs');
-const cp = require('child_process');
-
-if(fs.existsSync('../website.config.yaml'))
-  cp.fork('scripts/handle-config.js',{cwd:'..'});
 
 fs.copyFileSync('website.config.json','src/api/website.config.json');
 fs.copyFileSync('website.config.json','functions/website.config.json');
