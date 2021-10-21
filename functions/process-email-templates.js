a7d274316f1b85c88a93b778fd5edb9a5f9586ff
Mateus Rachid <mateus_rachid@outlook.com>
2021-10-21 10:47:04 -0300

    copy email templates over to functions directory

---
+++ functions/process-email-templates.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import config from './website.config.js';
+
+if(!config.emails || config.emails.templatesProcessed)
+  process.exit(0);
+
+fs.rmSync('email/templates', { recursive: true, force: true });
+fs.mkdirSync('email/templates',{recursive:true});
+
+for(const template of (config.emails?.templates||[])){
+  const src = path.resolve('../../',template.templateFile);
+  const dst = path.resolve('email/templates/',path.basename(template.templateFile));
+
+  fs.copyFileSync(src,dst);
+
+  template.templateFile = dst;
+}
+
+config.emails.templatesProcessed = true;
+
+fs.writeFileSync('website.config.json',JSON.stringify(
+  config
+));
